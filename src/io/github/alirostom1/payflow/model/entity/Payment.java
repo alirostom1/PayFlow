ecb4afc9b6665f40d1dffb118ebdacbf2be5966c
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-23 16:14:34 +0100

    PF-7 feat: add Payment entity as well as payment status enum https://alirostom2201.atlassian.net/jira/software/projects/PF/boards/101\?selectedIssue\=PF-7

---
+++ src/io/github/alirostom1/payflow/model/entity/Payment.java
@@ -0,0 +1,52 @@
+package io.github.alirostom1.payflow.model.entity;
+
+import java.time.LocalDateTime;
+
+import io.github.alirostom1.payflow.model.enums.Pstatus;
+
+public class Payment {
+    private String id;
+    private LocalDateTime dueDate;
+    private LocalDateTime paymentDate;
+    private String paymentType;
+    private Pstatus status;
+    private String subId;
+
+    public Payment(){}
+
+    public Payment(String id,LocalDateTime dueDate,LocalDateTime paymentDate,String paymentType,Pstatus status,String subId){
+        this.id = id;
+        this.dueDate = dueDate;
+        this.paymentDate = paymentDate;
+        this.paymentType = paymentType;
+        this.status = status;
+        this.subId = subId;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public LocalDateTime getDueDate(){
+        return this.dueDate;
+    }
+    public void setDueDate(LocalDateTime dueDate){
+        this.dueDate =  dueDate;
+    }
+    public LocalDateTime getPaymentDate(){
+        return this.paymentDate;
+    }
+    public String getPaymentType(){
+        return this.paymentType;
+    }
+    public void setPaymentType(String paymentType){
+        this.paymentType = paymentType;
+    }
+    public Pstatus getStatus(){
+        return this.getStatus();
+    }
+    public void setStatus(Pstatus status){
+        this.status = status;
+    }
+}
