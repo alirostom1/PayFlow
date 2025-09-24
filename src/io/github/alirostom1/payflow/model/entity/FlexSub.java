99cecac117887dba900f21586eae914ae1ebe4c6
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-24 07:46:46 +0100

    PF-9 fix: fix subs entities

--- src/io/github/alirostom1/payflow/model/entity/FlexSub.java
+++ src/io/github/alirostom1/payflow/model/entity/FlexSub.java
@@ -1,8 +1,16 @@
 package io.github.alirostom1.payflow.model.entity;
 
+import java.time.LocalDateTime;
+
+import io.github.alirostom1.payflow.model.enums.Sstatus;
 
 public class FlexSub extends Subscription {
     
+    public FlexSub(String id,String service,double price,LocalDateTime startDate,LocalDateTime endDate,Sstatus status){
+        super(id,service,price,startDate,endDate,status);
+    }
+
+
     public String toString(){
         return super.toString() + 
         "Subscription type : flexible";
