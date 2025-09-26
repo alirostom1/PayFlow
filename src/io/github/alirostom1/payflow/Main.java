47edbc00e73abe1723263f93b03f5b7095c66129
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-26 10:23:38 +0100

    test: implement main method to initialize database connection and services

--- src/io/github/alirostom1/payflow/Main.java
+++ src/io/github/alirostom1/payflow/Main.java
@@ -1,7 +1,25 @@
 package io.github.alirostom1.payflow;
 
+import java.sql.Connection;
+import java.time.LocalDateTime;
+
+import io.github.alirostom1.payflow.database.DatabaseConnection;
+import io.github.alirostom1.payflow.repository.PaymentRepository;
+import io.github.alirostom1.payflow.repository.SubscriptionRepository;
+import io.github.alirostom1.payflow.repository.Interface.PaymentRepositoryInterface;
+import io.github.alirostom1.payflow.repository.Interface.SubscriptionRepositoryInterface;
+import io.github.alirostom1.payflow.service.PaymentService;
+import io.github.alirostom1.payflow.service.SubscriptionService;
+import io.github.alirostom1.payflow.service.Interface.PaymentServiceInterface;
+import io.github.alirostom1.payflow.service.Interface.SubscriptionServiceInterface;
+
 public class Main{
     public final static void main(String[] args){
-        System.out.println("hello world");
+        Connection connection = DatabaseConnection.getInstance().getConnection();
+        SubscriptionRepositoryInterface subRepo = new SubscriptionRepository(connection);
+        PaymentRepositoryInterface payRepo = new PaymentRepository(connection);
+        PaymentServiceInterface payService = new PaymentService(payRepo);
+        SubscriptionServiceInterface subService = new SubscriptionService(subRepo, payService);
+        subService.createFixedSub("Netflix", 10, LocalDateTime.now(), 2);
     } 
 }
